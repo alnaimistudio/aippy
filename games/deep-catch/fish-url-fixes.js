9b4015a76103791c8e9910466c03f21d707c4b21
alnaimistudio <161966302+alnaimistudio@users.noreply.github.com>
2026-08-18 18:33:41 +0300

    Use repaired Deep Catch fish artwork

---
+++ games/deep-catch/fish-url-fixes.js
@@ -0,0 +1,7 @@
+window.FISH_ASSETS_DATA=window.FISH_ASSETS_DATA||{};
+Object.assign(window.FISH_ASSETS_DATA,{
+  'leviathan-whale':'assets/fish/leviathan-whale.webp?v=3.2',
+  'reef-glider':'assets/fish/reef-glider.webp?v=3.2',
+  'spike-puffer':'assets/fish/spike-puffer.webp?v=3.2',
+  'sunstripe-runner':'assets/fish/sunstripe-runner.webp?v=3.2'
+});
