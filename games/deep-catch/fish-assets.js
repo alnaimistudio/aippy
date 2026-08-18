06ac4c195ba1a7fa6de738fb19b858f839cc6699
alnaimistudio <161966302+alnaimistudio@users.noreply.github.com>
2026-08-18 14:59:48 +0300

    Wire Deep Catch fish assets

---
+++ games/deep-catch/fish-assets.js
@@ -0,0 +1,6 @@
+document.write('<script src="assets/fish/aqua-turtle.js?v=3.1"><\/script>');
+document.write('<script src="assets/fish/inferno-fangfish.js?v=3.1"><\/script>');
+document.write('<script src="assets/fish/leviathan-whale.js?v=3.1"><\/script>');
+document.write('<script src="assets/fish/moon-catfish.js?v=3.1"><\/script>');
+document.write('<script src="assets/fish/fish-pack-a.js?v=3.1"><\/script>');
+document.write('<script src="assets/fish/fish-pack-b.js?v=3.1"><\/script>');
